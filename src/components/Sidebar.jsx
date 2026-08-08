880c94db093b32158cfaebda9f6f26bcdd6bf4bf
Anshika Yadav <anshika.yadav0504@gmail.com>
2026-08-08 21:04:31 +0530

    add sidebar navigation and logout functionality

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { NavLink, useNavigate } from "react-router-dom";
+
+function Sidebar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <h2>Inventory Management</h2>
+
+      <nav>
+        <NavLink to="/dashboard">Dashboard</NavLink>
+        <br /><br />
+
+        <NavLink to="/products">Products</NavLink>
+        <br /><br />
+
+        <NavLink to="/users">Users</NavLink>
+        <br /><br />
+
+        <button onClick={handleLogout}>Logout</button>
+      </nav>
+    </div>
+  );
+}
+
+export default Sidebar;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,14 @@
+import Sidebar from "../components/Sidebar";
+
 function Dashboard() {
   return (
     <div>
+        <Sidebar/>
+
+        <main>
       <h1>Dashboard</h1>
       <p>Welcome to Inventory Management System</p>
+      </main>
     </div>
   );
 }
