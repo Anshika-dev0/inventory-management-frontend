288913780cd9496cabfc5ac4b8b609e2f01b7dc7
Anshika Yadav <anshika.yadav0504@gmail.com>
2026-08-11 20:52:32 +0530

    Add product Update Functionality

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,19 +3,45 @@ import API from "../services/api";
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [editingProduct, setEditingProduct] = useState(null);
 
   const fetchProducts = async () => {
     try {
       const response = await API.get("/products");
       setProducts(response.data);
     } catch (error) {
-      console.error("Error fetching products:", error);
+      console.error(error);
     }
   };
 
   useEffect(() => {
     fetchProducts();
   }, []);
+
+  const handleEdit = (product) => {
+    setEditingProduct(product);
+  };
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+
+    try {
+      await API.put(`/products/${editingProduct.id}`, {
+        name: editingProduct.name,
+        category: editingProduct.category,
+        price: Number(editingProduct.price),
+        quantity: Number(editingProduct.quantity),
+      });
+
+      alert("Product updated successfully");
+
+      setEditingProduct(null);
+      fetchProducts();
+    } catch (error) {
+      console.error(error);
+      alert("Failed to update product");
+    }
+  };
 
   return (
     <div>
@@ -27,10 +53,11 @@ function Products() {
         <table border="1" cellPadding="10">
           <thead>
             <tr>
-              <th>Product Name</th>
+              <th>Name</th>
               <th>Category</th>
               <th>Price</th>
               <th>Quantity</th>
+              <th>Action</th>
             </tr>
           </thead>
 
@@ -41,10 +68,89 @@ function Products() {
                 <td>{product.category}</td>
                 <td>₹{product.price}</td>
                 <td>{product.quantity}</td>
+                <td>
+                  <button type="button"
+                  onClick={() => {
+                    console.log("Edit Clicked",product);
+                    handleEdit(product);
+                  }}
+                  >
+                    Edit
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
+      )}
+
+      {editingProduct && (
+        <div>
+          <h2>Edit Product</h2>
+
+          <form onSubmit={handleUpdate}>
+            <input
+              type="text"
+              value={editingProduct.name}
+              onChange={(e) =>
+                setEditingProduct({
+                  ...editingProduct,
+                  name: e.target.value,
+                })
+              }
+            />
+
+            <br /><br />
+
+            <input
+              type="text"
+              value={editingProduct.category}
+              onChange={(e) =>
+                setEditingProduct({
+                  ...editingProduct,
+                  category: e.target.value,
+                })
+              }
+            />
+
+            <br /><br />
+
+            <input
+              type="number"
+              value={editingProduct.price}
+              onChange={(e) =>
+                setEditingProduct({
+                  ...editingProduct,
+                  price: e.target.value,
+                })
+              }
+            />
+
+            <br /><br />
+
+            <input
+              type="number"
+              value={editingProduct.quantity}
+              onChange={(e) =>
+                setEditingProduct({
+                  ...editingProduct,
+                  quantity: e.target.value,
+                })
+              }
+            />
+
+            <br /><br />
+
+            <button type="submit">Update Product</button>
+
+            <button
+              type="button"
+              onClick={() => setEditingProduct(null)}
+            >
+              Cancel
+            </button>
+          </form>
+        </div>
       )}
     </div>
   );
